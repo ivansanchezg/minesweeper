61a1a1b2ebba2db2905e4d0e5f8c87bd05004e9a
ivansanchezg <ivan.sanchez@wizeline.com>
2017-06-19 10:23:39 -0500

    Added time count

--- src/main/java/com/ivansanchezg/minesweeper/GraphicUI.java
+++ src/main/java/com/ivansanchezg/minesweeper/GraphicUI.java
@@ -4,14 +4,23 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.Timer;
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+
+import java.io.IOException;
 
 public class GraphicUI {
     private JFrame frame;
@@ -19,6 +28,7 @@ public class GraphicUI {
     private Board board;
     private int rows;
     private int cols;
+    private int flagsCounter;
 
     private int tileSize = 20;
     private JButton[][] buttonsMatrix;
@@ -26,14 +36,17 @@ public class GraphicUI {
     private int widthOffset = tileSize/2;
     private int heightOffset = 75;
 
-    JLabel minesLabel;
-    JLabel tilesRemainingLabel;
+    private Image flag;
+    private JLabel flagsLabel;
+    private JLabel timeLabel;
+
+    private int time;
+    private Timer timer;
 
     public GraphicUI() {
         frame = new JFrame("MineSweeper");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(true);
-        frame.setBackground(Color.RED);
+        frame.setResizable(false);
         panel = new JPanel();
         panel.setLayout(null);
         frame.add(panel);
@@ -41,21 +54,46 @@ public class GraphicUI {
     }
 
     public void initialize(int rows, int cols) {
-        System.out.println("Initialize: " + rows + ", " + cols);
         this.rows = rows;
         this.cols = cols;
+        this.flagsCounter = 0;
+        this.time = 0;
         board = new Board(rows,cols);
         buttonsMatrix = new JButton[rows][cols];
         initializeWindow();
         initializeGraphicalComponents();
         displayWindow();
+        startTimeCount();
     }
 
-     private void initializeWindow() {
+    private void initializeWindow() {
         frame.setMinimumSize(new Dimension((cols + 1) * tileSize, (rows + 2) * tileSize + heightOffset));
     }
 
+    private void startTimeCount() {
+        if(timer != null) {
+            timer.stop();
+        }
+        ActionListener taskPerformer = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                time += 1;
+                timeLabel.setText("Time: " +  time);
+            }
+        };
+        timer = new Timer(1000, taskPerformer);
+        timer.start();
+    }
+
     private void initializeGraphicalComponents() {
+        try {
+            flag = ImageIO.read(getClass().getResource("/images/flag.png"));
+        } catch(IOException e) {
+            System.out.println("IOException. Couldn't load image'");
+            System.exit(1);
+        } catch(Exception e) {
+            System.out.println("Exception. Couldn't load image'");
+            System.exit(1);
+        }
         panel.removeAll();
         panel.setSize((cols + 1) * tileSize, (rows + 2) * tileSize + heightOffset);
         initializeConfig(panel);
@@ -87,13 +125,15 @@ public class GraphicUI {
         newButton.addActionListener(new NewGameActionListener(hTextField, wTextField, this));
         panel.add(newButton);
 
-        minesLabel = new JLabel("Mines: " + board.getMines());
-        minesLabel.setBounds(10, 40, 100, 20);
-        panel.add(minesLabel);
+        // Flags
+        flagsLabel = new JLabel("Flags: " + String.valueOf(board.getMines() - flagsCounter));
+        flagsLabel.setBounds(10, 40, 100, 20);
+        panel.add(flagsLabel);
 
-        tilesRemainingLabel = new JLabel("Tiles: " + (rows * cols - board.getTilesRevealed() - board.getMines()));
-        tilesRemainingLabel.setBounds(120, 40, 100, 20);
-        panel.add(tilesRemainingLabel);
+        // Time
+        timeLabel = new JLabel("Time: " + time);
+        timeLabel.setBounds(120, 40, 100, 20);
+        panel.add(timeLabel);
     }
 
     private void initializeButtons(JPanel panel, int rows, int cols) {
@@ -104,7 +144,8 @@ public class GraphicUI {
                 button.setBounds((col * tileSize) + widthOffset, 
                                  (row * tileSize) + heightOffset, 
                                  tileSize, tileSize);
-                button.addActionListener(new TileActionListener(button, board, this));
+                //button.addActionListener(new TileActionListener(button, board, this));
+                button.addMouseListener(new ButtonMouseListener(button, board, this));
                 buttonsMatrix[row][col] = button;
                 panel.add(button);
             }
@@ -139,7 +180,7 @@ public class GraphicUI {
             }
         }
     
-        if(board.isGameOver() || board.playerWon()) {
+        if (board.isGameOver() || board.playerWon()) {
             for (int row = 0; row < rows; row++) {
                 for (int col = 0; col < cols; col++) {
                     buttonsMatrix[row][col].setEnabled(false);
@@ -147,8 +188,51 @@ public class GraphicUI {
             }
         }
         
-        tilesRemainingLabel.setText("Tiles: " +  (rows * cols - board.getTilesRevealed() - board.getMines()));
+        //timeLabel.setText("Time: " +  this.time);
+        flagsLabel.setText("Flags: " + String.valueOf(board.getMines() - flagsCounter));
         panel.repaint();
+    }
+
+    public void toggleFlag(int row, int col) {
+        if (buttonsMatrix[row][col].getIcon() == null) {
+            buttonsMatrix[row][col].setIcon(new ImageIcon(flag));
+            flagsCounter++;
+        } else {
+            buttonsMatrix[row][col].setIcon(null);
+            flagsCounter--;
+        }
+        checkRevealResult(); //?
+    }
+
+    private class ButtonMouseListener implements MouseListener {
+        private JButton button;
+        private Board board;
+        private GraphicUI graphicUI;
+
+        public ButtonMouseListener(JButton button, Board board, GraphicUI graphicUI) {
+            this.button = button;
+            this.board = board;
+            this.graphicUI = graphicUI;
+        }
+
+        public void mouseClicked(MouseEvent event) {
+            int row = (button.getY() - heightOffset)/tileSize;
+            int col = (button.getX() - widthOffset)/tileSize;
+            //System.out.println("Clicked on button " + row + "," + col);
+            if(SwingUtilities.isLeftMouseButton(event)) {
+                //System.out.println("Left mouse pressed!");
+                board.reveal(row, col);
+                graphicUI.checkRevealResult();
+            } else if(SwingUtilities.isRightMouseButton(event)) {
+                //System.out.println("Right mouse pressed!");
+                graphicUI.toggleFlag(row, col);
+            }
+        }
+
+        public void mouseEntered(MouseEvent event) {}
+        public void mouseExited(MouseEvent event) {}
+        public void mousePressed(MouseEvent event) {}
+        public void mouseReleased(MouseEvent event) {}
     }
 
     private class NewGameActionListener implements ActionListener {
@@ -176,22 +260,23 @@ public class GraphicUI {
         }
     }
 
-    private class TileActionListener implements ActionListener {
-        private JButton button;
-        private Board board;
-        private GraphicUI graphicUI;
+    // private class TileActionListener implements ActionListener {
+    //     private JButton button;
+    //     private Board board;
+    //     private GraphicUI graphicUI;
 
-        public TileActionListener(JButton button, Board board, GraphicUI graphicUI) {
-            this.button = button;
-            this.board = board;
-            this.graphicUI = graphicUI;
-        }
-        public void actionPerformed(ActionEvent e) {
-            int row = (button.getY() - heightOffset)/tileSize;
-            int col = (button.getX() - widthOffset)/tileSize;
-            System.out.println("\nClicked on button " + row + "," + col);
-            board.reveal(row, col);
-            graphicUI.checkRevealResult();
-        }
-    }
+    //     public TileActionListener(JButton button, Board board, GraphicUI graphicUI) {
+    //         this.button = button;
+    //         this.board = board;
+    //         this.graphicUI = graphicUI;
+    //     }
+    //     public void actionPerformed(ActionEvent e) {
+    //         System.out.println("actionPerformed");
+    //         int row = (button.getY() - heightOffset)/tileSize;
+    //         int col = (button.getX() - widthOffset)/tileSize;
+    //         System.out.println("Clicked on button " + row + "," + col);
+    //         board.reveal(row, col);
+    //         graphicUI.checkRevealResult();
+    //     }
+    // }
 }
